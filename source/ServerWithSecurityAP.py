5ad7005a01e090d77c86ee2cab687a0b83c06242
Harsh <harshhareendran58@gmail.com>
2024-08-03 14:33:51 -0400

    feat: Complete Task 1

---
+++ source/ServerWithSecurityAP.py
@@ -0,0 +1,144 @@
+import pathlib
+import socket
+import sys
+import time
+from datetime import datetime
+import secrets
+import traceback
+from signal import signal, SIGINT
+from cryptography import x509
+from cryptography.exceptions import InvalidSignature
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+
+
+def convert_int_to_bytes(x):
+    """
+    Convenience function to convert Python integers to a length-8 byte representation
+    """
+    return x.to_bytes(8, "big")
+
+
+def convert_bytes_to_int(xbytes):
+    """
+    Convenience function to convert byte value to integer value
+    """
+    return int.from_bytes(xbytes, "big")
+
+
+def read_bytes(socket, length):
+    """
+    Reads the specified length of bytes from the given socket and returns a bytestring
+    """
+    buffer = []
+    bytes_received = 0
+    while bytes_received < length:
+        data = socket.recv(min(length - bytes_received, 1024))
+        if not data:
+            raise Exception("Socket connection broken")
+        buffer.append(data)
+        bytes_received += len(data)
+
+    return b"".join(buffer)
+
+def load_server_private_key():
+    with open("source/auth/server_private_key.pem", "rb") as key_file:
+        return serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+
+def load_ca_certificate():
+    with open("source/auth/cacsertificate.crt", "rb") as cert_file:
+        return x509.load_pem_x509_certificate(cert_file.read(), default_backend())
+
+
+def load_server_certificate():
+    with open("source/auth/server_signed.crt", "rb") as cert_file:
+        return cert_file.read()
+
+
+def main(args):
+    port = int(args[0]) if len(args) > 0 else 4321
+    address = args[1] if len(args) > 1 else "localhost"
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((address, port))
+            s.listen()
+
+            client_socket, client_address = s.accept()
+            with client_socket:
+                while True:
+                    match convert_bytes_to_int(read_bytes(client_socket, 8)):
+                        case 0:
+                            # If the packet is for transferring the filename
+                            print("Receiving file...")
+                            filename_len = convert_bytes_to_int(
+                                read_bytes(client_socket, 8)
+                            )
+                            filename = read_bytes(
+                                client_socket, filename_len
+                            ).decode("utf-8")
+                            # print(filename)
+                        case 1:
+                            # If the packet is for transferring a chunk of the file
+                            start_time = time.time()
+
+                            file_len = convert_bytes_to_int(
+                                read_bytes(client_socket, 8)
+                            )
+                            file_data = read_bytes(client_socket, file_len)
+                            # print(file_data)
+
+                            filename = "recv_" + filename.split("/")[-1]
+
+                            # Write the file with 'recv_' prefix
+                            with open(
+                                f"recv_files/{filename}", mode="wb"
+                            ) as fp:
+                                fp.write(file_data)
+                            print(
+                                f"Finished receiving file in {(time.time() - start_time)}s!"
+                            )
+                        case 2:
+                            # Close the connection
+                            # Python context used here so no need to explicitly close the socket
+                            print("Closing connection...")
+                            s.close()
+                            break
+                        case 3:
+                            auth_message_len = convert_bytes_to_int(read_bytes(client_socket, 8))
+                            auth_message = read_bytes(client_socket, auth_message_len)
+
+                            server_private_key = load_server_private_key()
+                            signed_auth_message = server_private_key.sign(
+                                auth_message,
+                                padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
+                                hashes.SHA256()
+                            )
+
+                            client_socket.sendall(convert_int_to_bytes(len(signed_auth_message)))
+                            client_socket.sendall(signed_auth_message)
+
+                            server_certificate = load_server_certificate()
+                            client_socket.sendall(convert_int_to_bytes(len(server_certificate)))
+                            client_socket.sendall(server_certificate)
+
+
+    except Exception as e:
+        print(e)
+        s.close()
+
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    exit(0)
+    
+if __name__ == "__main__":
+    # Tell Python to run the handler() function when SIGINT is recieved
+    signal(SIGINT, handler)
+    main(sys.argv[1:])
